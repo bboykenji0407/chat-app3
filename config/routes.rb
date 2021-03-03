a7e58727aa6e9c90c65af50afad1e5f2c20f81c8
bboykenji0407 <bboy.kenji.flex@gmail.com>
2021-03-03 19:01:14 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
-  root to: "messages#index"  #仮のルーティングを設定
+  root to: "rooms#index"  #仮のルーティングを設定
   resources :users, only: [:edit, :update ] #usersのビュー表示用のルーティングを設定
   resources :rooms, only: [:new, :create ]  #roomsの新規作成画を面表示されるビューのルーティング設定
 end
