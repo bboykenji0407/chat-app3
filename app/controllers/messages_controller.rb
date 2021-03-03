1989d962a43768b1e3ad86e228e286d929ba8278
bboykenji0407 <bboy.kenji.flex@gmail.com>
2021-03-03 21:58:33 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,25 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
+  end
+
+  def create          #messagesコントローラーにcreateアクションを定義します。
+    @room = Room.find(params[:room_id])   #@room.messages.newでチャットルームに紐づいたメッセージのインスタンスを生成し、message_paramsを引数にして、privateメソッドを呼び出します。
+    @message = @room.messages.new(message_params)   #生成したインスタンスを@messageに代入し、saveメソッドでメッセージの内容をmessagesテーブルに保存します。
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+     render :index
+    end      #createアクション内に、メッセージを保存できた場合とできなかった場合で条件分岐の処理を行います。
+  end
+  private
+  
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
+
+
+#パラメーターの中に、ログインしているユーザーのidと紐付いている、メッセージの内容contentを受け取れるように許可します。
+#privateメソッドとしてmessage_paramsを定義し、メッセージの内容contentをmessagesテーブルへ保存できるようにします。
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
   root to: "rooms#index"  #仮のルーティングを設定
-  resources :users, only: [:edit, :update ] #usersのビュー表示用のルーティングを設定
-  resources :rooms, only: [:new, :create ]  #roomsの新規作成画を面表示されるビューのルーティング設定
+  resources :users, only: [:edit, :update ]  #usersのビュー表示用のルーティングを設定
+  resources :rooms, only: [:new, :create ] do #roomsの新規作成画を面表示されるビューのルーティング設定
+    resources :messages, only: [:index, :create]
+  end
 end
