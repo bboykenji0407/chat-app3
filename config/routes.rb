ec3f63a8f910c78a4b50a8ee1cb44eba25474c84
bboykenji0407 <bboy.kenji.flex@gmail.com>
2021-03-03 17:08:57 +0900

    room_usersテーブルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get 'messages/index'
   root to: "messages#index"  #仮のルーティングを設定
   resources :users, only: [:edit, :update ] #usersのビュー表示用のルーティングを設定
-  resources :rooms, only: [:new, :create ]  #roomsの表示されるビューの設定
+  resources :rooms, only: [:new, :create ]  #roomsの新規作成画を面表示されるビューのルーティング設定
 end
